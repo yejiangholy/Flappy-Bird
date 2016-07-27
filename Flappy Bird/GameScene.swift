1ebf5e83b18359b24195f702570b9de10480ef65
Carter Jiang <yejiangholy@gmail.com>
2016-07-27 13:24:13 -0400

    random scaled static pipe

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -56,9 +56,37 @@ class GameScene: SKScene {
         
         bird.runAction(makeBirdFlap)
         
+        //add physics body
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture.size().height / 2)
+        bird.physicsBody!.dynamic = true
+        bird.setScale(0.7)
         self.addChild(bird)
         
+        let ground = SKNode()
+        ground.position = CGPointMake(0, 0)
+        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, 1))
+        ground.physicsBody!.dynamic = false
+        self.addChild(ground)
         
+        
+        let gapHeight = birdTexture.size().height * 4
+        // generate random number up to some value
+        let movementAmount = arc4random() % UInt32(self.frame.height / 2)
+        let pipeOffset = CGFloat(movementAmount) - self.frame.size.height / 4
+        
+        
+        
+        let  pipeTexture = SKTexture(imageNamed: "pipe1.png")
+        let pipe1 = SKSpriteNode(texture: pipeTexture)
+        pipe1.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame) + (pipeTexture.size().height * 0.3) / 2 + (gapHeight/2) + pipeOffset)
+         pipe1.setScale(0.3)
+        self.addChild(pipe1)
+        
+        let  pipe2Texture = SKTexture(imageNamed: "pipe2.png")
+        let pipe2 = SKSpriteNode(texture: pipe2Texture)
+        pipe2.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame) - (pipe2Texture.size().height * 0.3 ) / 2 - (gapHeight/2) + pipeOffset)
+       pipe2.setScale(0.3)
+        self.addChild(pipe2)
         
     }
     
@@ -68,6 +96,8 @@ class GameScene: SKScene {
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
+        bird.physicsBody!.velocity = CGVectorMake(0, 0)
+        bird.physicsBody!.applyImpulse(CGVectorMake(0, 50))
         
         
           }
